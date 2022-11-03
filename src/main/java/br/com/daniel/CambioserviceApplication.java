5c9c6f83b5ee96add836504e39e9596c8b1c2dbe
Daniel Celestino <danielcelestino@gmail.com>
2022-11-02 22:21:27 -0300

    Project created

---
+++ src/main/java/br/com/daniel/CambioserviceApplication.java
@@ -0,0 +1,13 @@
+package br.com.daniel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CambioserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CambioserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/daniel/CambioserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.daniel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CambioserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
