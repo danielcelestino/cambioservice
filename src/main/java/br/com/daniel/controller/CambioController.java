2af63962be0580dec47d1e59d0fc46b7effda1f4
Daniel Celestino <danielcelestino@gmail.com>
2022-12-17 12:17:07 -0300

    Implements swagger ui

--- src/main/java/br/com/daniel/controller/CambioController.java
+++ src/main/java/br/com/daniel/controller/CambioController.java
@@ -12,7 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.daniel.model.Cambio;
 import br.com.daniel.repository.CambioRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
+@Tag(name = "Cambio Service API")
 @RestController
 @RequestMapping(value = "cambio-service")
 public class CambioController {
@@ -23,6 +26,7 @@ public class CambioController {
 	@Autowired
 	private CambioRepository repository;
 	
+	@Operation(summary = "Get Cambio from currecy")
 	@GetMapping(value = "/{amount}/{from}/{to}")
 	public Cambio getCambio(
 			@PathVariable("amount") BigDecimal amount,
